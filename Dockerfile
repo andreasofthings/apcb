da85dec65d449fb8daa0d89ba340a58111cc8438
Andreas Neumeier <andreas@neumeier.org>
2017-12-10 11:02:16 +0100

    Move to Alpine & GCR

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.6
+FROM python:alpine
 ENV PYTHONUNBUFFERED 1
 ADD requirements.txt .
 RUN pip install -r requirements.txt && adduser --disabled-password --gecos '' appuser
--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
-docker build .
-docker push andreasneumeier/apcb:latest
+
+# use Google Cloud Registry
+docker tag andreasneumeier/apc:latest eu.gcr.io/pramari-de/apc
+gcloud docker -- push eu.gcr.io/pramari-de/apc
+
+# docker build .
+# docker push andreasneumeier/apcb:latest
