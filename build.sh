816356d051043855896f44a60050871c8b772e0d
Andreas Neumeier <andreas@neumeier.org>
2017-12-10 15:00:37 +0100

    updated buildscipt.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # use Google Cloud Registry
-docker tag andreasneumeier/apc:latest eu.gcr.io/pramari-de/apc
-gcloud docker -- push eu.gcr.io/pramari-de/apc
+docker tag andreasneumeier/apc:latest eu.gcr.io/pramari-de/apcb
+gcloud docker -- push eu.gcr.io/pramari-de/apcb
 
 # docker build .
 # docker push andreasneumeier/apcb:latest
