c39f7e6e96f850537115bd6aaa042b7cef73cc52
Andreas Neumeier <andreas@neumeier.org>
2018-09-11 21:00:54 +0200

    Oh boy.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # use Google Cloud Registry
-docker image build . -t andreasneumeier/apcb:latest
+docker image build . -t andreasneumeier/apcb:latest --no-cache
 docker tag andreasneumeier/apcb:latest eu.gcr.io/pramari-de/apcb
 gcloud docker -- push eu.gcr.io/pramari-de/apcb
 
